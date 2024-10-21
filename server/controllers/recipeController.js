c3a33799844c11d7a7b576eefd601ab9fdbdf76e
Sasmitha Kannangara <141046317+Sasmitha000@users.noreply.github.com>
2024-10-21 12:17:40 +0530

    Update recipeController.js

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -8,3 +8,7 @@
 exports.homepage = async(req, res) => {
   res.render('index', { title: 'Recipe Book - Home' });
 }
+
+exports.recipePage = (req, res) => {
+  res.render('recipe', { title: 'Dummy Recipe Page' });  // Render 'recipe.ejs'
+};
